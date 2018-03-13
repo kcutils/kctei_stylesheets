2529dcada1e8d99bec4de701713e2f358c1df87c
S. Fischer <sfis@informatik.uni-kiel.de>
2018-03-13 12:55:37 +0100

    wrapper script for saxon using java for windows added

---
+++ xsltproc.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+SET XSLT_PROC_JAR=lib\saxon9he.jar
+SET JAVA=C:\ProgramData\Oracle\Java\javapath\java.exe
+
+IF NOT EXIST %JAVA% (
+  ECHO "Java not found!"
+  EXIT /b 1
+)
+
+IF NOT EXIST %XSLT_PROC_JAR% (
+  ECHO "XSLT processor JAR %XSLT_PROC_JAR% not found!"
+  EXIT /b 1
+)
+  
+%JAVA% -jar "%XSLT_PROC_JAR%" %*
+
