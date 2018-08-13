217c2d701e03149891ae05309d12b7e7f9ed1392
S. Fischer <sfis@informatik.uni-kiel.de>
2018-08-13 12:40:53 +0200

    Archive creation script renamed, better error handling

---
+++ create_final_archive.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SCRIPT=$( readlink -f "$0" )
+SCRIPT_PATH=$( dirname "$SCRIPT" )
+
+cd ${SCRIPT_PATH}/..
+
+echo "Creating archive ..."
+
+rm -f kctei_stylesheets/kctei_stylesheets.zip
+
+OUT=$( zip -r kctei_stylesheets/kctei_stylesheets.zip kctei_stylesheets/LICENSE kctei_stylesheets/README* kctei_stylesheets/xslt/* kctei_stylesheets/xsltproc* kctei_stylesheets/data kctei_stylesheets/lib kctei_stylesheets/convert.sh -x \*.git* -x\*saxon9he.jar 2>&1 )
+
+if [ $? -ne 0 ]; then
+  echo "Error while creating final archive!"
+  echo "$OUT"
+  echo "Exitting ..."
+  exit 1
+fi
+
--- create_zip_archive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ..
-zip -r kctei_stylesheets/kctei_stylesheets.zip kctei_stylesheets/LICENSE kctei_stylesheets/README* kctei_stylesheets/xslt/* kctei_stylesheets/xsltproc* kctei_stylesheets/data kctei_stylesheets/lib kctei_stylesheets/convert.sh -x \*.git* -x\*saxon9he.jar
-cd -
